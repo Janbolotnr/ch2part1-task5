500018d93dd7b4d36225094dc5f9f25585e01bfe
Janbolotnr <janbolotnr@gmail.com>
2020-03-08 00:56:24 +0600

    ch2task5

---
+++ decision5.py
@@ -0,0 +1,16 @@
+a = int(input("chislo st: "))
+b = int(input("chislo st2: "))
+c = int(input("chislo st3: "))
+result1 = (a // 2 + a % 2)
+result2 = (b // 2 + b % 2)
+result3 = (c // 2 + c % 2)
+summ = (result1 + result2 + result3)
+#result = (a // 2 + b // 2 + c // 2 + a % 2 + b % 2 + c % 2)
+
+print(result1, result2, result3)
+print(summ)
+
+
+
+
+
